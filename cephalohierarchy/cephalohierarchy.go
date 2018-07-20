48c3e6a7167b3ca6f88f7cbb47c0f7805423f30b
paulidealiste <paulidealiste@aol.com>
2018-07-20 15:53:19 +0200

    Initial idea hierarchy

--- cephalohierarchy/cephalohierarchy.go
+++ cephalohierarchy/cephalohierarchy.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/paulidealiste/Cephalopod/cephalobjects"
+	"github.com/paulidealiste/Cephalopod/cephalostructures"
 	"github.com/paulidealiste/Cephalopod/cephaloutils"
 )
 
@@ -13,16 +14,32 @@ func HierarchicalClustering(input *cephalobjects.DataStore) {
 }
 
 func constructTree(dmc cephalobjects.DataMatrix) {
-	var dmi cephalobjects.DataMatrix
-	dmi.Variables = make([]string, len(dmc.Variables))
-	dmm := cephaloutils.DataMatrixMin(dmc, true, false)
-	for {
-		if len(dmi.Variables) > 1 {
-			fmt.Println(dmm)
-			// dmm = cephaloutils.DataMatrixMin(dmi, true, false)
-		} else {
-			//Should break here
+	squarePusher := cephalostructures.Stack{}
+	var dmm cephalobjects.DataMatrixExtreme
+	dmm = cephaloutils.DataMatrixMin(dmc, true, false)
+	squarePusher.Push(dmm)
+	transformDataMatrix(&dmc, dmm)
+	fmt.Println(dmc.Matrix)
+}
+
+func transformDataMatrix(dmc *cephalobjects.DataMatrix, dmm cephalobjects.DataMatrixExtreme) {
+	pilaf, nlab := connectNearestLabels(dmc.Variables, dmm.RowName, dmm.ColName)
+	dmc.Variables = pilaf
+	inmat := make([][]float64, len(dmc.Variables)-1)
+	fmt.Println(inmat)
+	fmt.Println(dmc.Variables)
+	fmt.Println(nlab)
+}
+
+func connectNearestLabels(varlabs []string, rc string, cc string) ([]string, string) {
+	inlabs := make([]string, 0)
+	nstrg := rc + "," + cc
+	for _, lab := range varlabs {
+		if lab != rc && lab != cc {
+			inlabs = append(inlabs, lab)
+		} else if lab == cc {
+			inlabs = append(inlabs, nstrg)
 		}
-		break
 	}
+	return inlabs, nstrg
 }
